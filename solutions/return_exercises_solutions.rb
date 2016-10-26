2f160fd22494a01f70fe02ae69821a23a763eb0c
Ryan <Hall.Ryan.R@gmail.com>
2016-10-25 17:24:44 -0700

    hotfix

---
+++ solutions/return_exercises_solutions.rb
@@ -0,0 +1,20 @@
+
+# 2.
+  def times_three_and_plus_five(n)
+    return (n * 3) + 5
+  end
+
+
+# 3.
+  def times_three_and_plus_five(n)
+    (n * 3) + 5
+  end
+
+
+# 5.
+  def even_or_odd(n)
+    if n.even?
+      return "even"
+    end
+    "odd"
+  end
